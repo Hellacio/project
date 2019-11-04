8218158fb54e4e87184bf32fe10473e986a24b09
niko nousiainen <niko.nousiainen@metropolia.fi>
2019-11-05 01:06:02 +0200

    more swarm stuff

--- swarm-managers.tf
+++ swarm-managers.tf
@@ -22,20 +22,35 @@ resource "google_compute_instance" "managers" {
   provisioner "remote-exec" {
     inline = [
       "sudo apt-get update",
-      "sudo apt-get install apt-transport-https ca-certificates curl software-properties-common",
+      "sudo apt-get -y install apt-transport-https",
+      "sudo apt-get -y install ca-certificates",
+      "sudo apt-get -y install curl",
+      "sudo apt-get -y install gnupg-agent",
+      "sudo apt-get -y install software-properties-common",
       "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
-      "sudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable'",
+      "sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\"",
       "sudo apt-get update",
-      "sudo apt-get install docker-ce",
-      "sudo systemctl start docker",
-      "sudo systemctl enable docker",
-      "curl -L 'https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)' -o /usr/local/bin/docker-compose",
-      "sudo chmod +x /usr/local/bin/docker-compose"
+      "sudo apt-get -y install docker-ce",
+      "sudo apt-get -y install docker-ce-cli", 
+      "sudo apt-get -y install containerd.io",
+      "sudo usermod -aG docker ubuntu",
+      "sudo curl -L \"https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)\" -o /usr/local/bin/docker-compose",
+      "sudo chmod +x /usr/local/bin/docker-compose",
+      "echo ${google_compute_instance.managers.0.network_interface.0.network_ip}:2377",
+      "sudo docker swarm init --advertise-addr ${google_compute_instance.managers.0.network_interface.0.network_ip}:2377"
     ]
     connection  {
       host = "${google_compute_instance.managers.0.network_interface.0.access_config.0.nat_ip}"
       user = "${var.ssh_user}"
       private_key = "${file("id_rsa")}"
     }
+  }
+}
+data "external" "swarm_tokens" {
+  program = ["fetch_tokens.sh"]
+
+  query = {
+    host = "${google_compute_instance.workers.0.network_interface.0.access_config.0.nat_ip}"
+    user = "${var.ssh_user}"
   }
 }
